98fc4ac67685db1a55406bbbad11dfb7f7a3febb
Ahmad Ali <xevenahmad@gmail.com>
2024-10-17 12:48:21 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import { Container } from "./components/styles/Container.style";
+import Header from "./components/Header";
+function App() {
+  return (
+    <>
+      <Header />
+      <Container>
+        <h1>Hello</h1>
+      </Container>
+    </>
+  );
+}
+
+export default App;
